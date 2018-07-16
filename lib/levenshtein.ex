a31c6fc13b2add8abad405db2b13645508918edd
preciz <barna.kovacs.codes@gmail.com>
2018-07-16 18:09:44 +0200

    Wrap :levenshtein.distance_cached

--- lib/levenshtein.ex
+++ lib/levenshtein.ex
@@ -9,4 +9,15 @@ defmodule Levenshtein do
   def distance(a, b) when is_list(a) and is_list(b) do
     :levenshtein.distance(a, b)
   end
+
+  def distance_cached(a, b) when is_binary(a) and is_binary(b) do
+    distance(
+      a |> String.to_charlist,
+      b |> String.to_charlist
+    )
+  end
+
+  def distance_cached(a, b) when is_list(a) and is_list(b) do
+    :levenshtein.distance_cached(a, b)
+  end
 end
