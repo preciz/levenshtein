eb14ab2b7755c59f0d280cf00871dc1670b30b2e
preciz <barna.kovacs.codes@gmail.com>
2018-07-16 18:00:43 +0200

    Make mix.exs hex.pm compilant

--- mix.exs
+++ mix.exs
@@ -7,17 +7,28 @@ defmodule Levenshtein.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
+      name: "Levenshtein",
+      source_url: "https://github.com/preciz/levenshtein"
     ]
   end
 
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: []
     ]
   end
 
   defp deps do
     []
+  end
+
+  defp package() do
+    [
+      maintainers: ["Barna Kovacs"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/preciz/levenshtein"}
+    ]
   end
 end
