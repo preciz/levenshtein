d2834d038a7314488ab7bc70a7353e5de97f4cb4
preciz <barna.kovacs.codes@gmail.com>
2019-06-20 20:53:02 +0200

    Add basic tests

---
+++ test/levenshtein_test.exs
@@ -0,0 +1,17 @@
+defmodule SimilarityTest do
+  use ExUnit.Case
+
+  test "distance" do
+    assert Levenshtein.distance("", "") == 0
+    assert Levenshtein.distance("a", "a") == 0
+    assert Levenshtein.distance("a", "b") == 1
+    assert Levenshtein.distance("alma", "korte") == 5
+  end
+
+  test "distance_cached" do
+    assert Levenshtein.distance_cached("", "") == 0
+    assert Levenshtein.distance_cached("a", "a") == 0
+    assert Levenshtein.distance_cached("a", "b") == 1
+    assert Levenshtein.distance_cached("alma", "korte") == 5
+  end
+end
